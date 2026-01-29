c189c8e516bfadf0df57c79cf826a52c7577d7d0
Naftaly Souza <naft.dev@proton.me>
2026-01-29 14:59:11 -0300

    sqrt "function"

--- miniProjects/Project1/Program.cs
+++ miniProjects/Project1/Program.cs
@@ -1,41 +1,53 @@
 ﻿// at this point I haven't learned if statments, loops or user input
 // in C# yet but I've decided to try
 
+using System.Diagnostics;
+
 Console.WriteLine("Welcome to your Super Calculator");
 Console.WriteLine("Choose an option:");
-Console.WriteLine("[1] Sum  [2] Difference  [3] Product  [0] Quit ");
-//  [4] Int Quotient   [5] Decimal Quotient   [6] Modulus
+Console.WriteLine("[1] Sum  [2] Difference  [3] Product  [7] Square Root  [0] Quit ");
+//  [4] Quotient   [5] Modulus  [6] Power  [7] Square Root
 
 // The Console.ReadLine() method returns a string, use Convert method
 int userOption = Convert.ToInt32(Console.ReadLine());
+double firstNumber = 0;
+double secondNumber = 0;
 
 // for loop in future
 if (userOption != 0)
 {
-    if (userOption == 1)
+    if (userOption != 7)
     {
-        Console.WriteLine("Pls give 2 numbers: ");
-        int firstNumber = Convert.ToInt32(Console.ReadLine());
-        int secondNumber = Convert.ToInt32(Console.ReadLine());
-        int result = firstNumber + secondNumber;
-        Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+        Console.WriteLine("Enter the first number:");
+        firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number:");
+        secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        if (userOption == 1)
+        {
+            double result = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+        }
+        else if (userOption == 2)
+        {
+            double result = firstNumber - secondNumber;
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+        }
+        else if (userOption == 3)
+        {
+            double result = firstNumber * secondNumber;
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+        }
     }
-    else if (userOption == 2)
+    else if (userOption == 7)
     {
-        Console.WriteLine("Pls give 2 numbers: ");
-        int firstNumber = Convert.ToInt32(Console.ReadLine());
-        int secondNumber = Convert.ToInt32(Console.ReadLine());
-        int result = firstNumber - secondNumber;
-        Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
-    }
-    else if (userOption == 3)
-    {
-        Console.WriteLine("Pls give 2 numbers: ");
-        int firstNumber = Convert.ToInt32(Console.ReadLine());
-        int secondNumber = Convert.ToInt32(Console.ReadLine());
-        int result = firstNumber * secondNumber;
-        Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+        Console.WriteLine("Enter the number:");
+        firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        double result = System.Math.Sqrt(firstNumber);
+        Console.WriteLine($"√{firstNumber} = {result}");
     }
 
 }
+
 // else {break;}
--- miniProjects/Project1/obj/Debug/net10.0/Project1.AssemblyInfo.cs
+++ miniProjects/Project1/obj/Debug/net10.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6285dc46b79f56c9ce35fe7c1ddfeae1a5c33f00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
