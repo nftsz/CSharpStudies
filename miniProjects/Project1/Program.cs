791cdce556ffe455d12a3acc87a4d1dbe21d52d3
Naftaly Souza <naft.dev@proton.me>
2026-01-31 10:32:43 -0300

    switch case

--- miniProjects/Project1/Program.cs
+++ miniProjects/Project1/Program.cs
@@ -1,53 +1,65 @@
-﻿// at this point I haven't learned if statments, loops or user input
+﻿// at this point I haven't learned if statments, switch, loops or user input
 // in C# yet but I've decided to try
-
-using System.Diagnostics;
 
 Console.WriteLine("Welcome to your Super Calculator");
 Console.WriteLine("Choose an option:");
-Console.WriteLine("[1] Sum  [2] Difference  [3] Product  [7] Square Root  [0] Quit ");
+Console.WriteLine("[1] Sum  [2] Difference  [3] Product  [4] Quotient  [5] Modulus  [6] Power  [7] Square Root  [0] Quit ");
 //  [4] Quotient   [5] Modulus  [6] Power  [7] Square Root
 
 // The Console.ReadLine() method returns a string, use Convert method
 int userOption = Convert.ToInt32(Console.ReadLine());
-double firstNumber = 0;
-double secondNumber = 0;
 
-// for loop in future
-if (userOption != 0)
+
+if (userOption == 0)
 {
-    if (userOption != 7)
-    {
-        Console.WriteLine("Enter the first number:");
-        firstNumber = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Enter the second number:");
-        secondNumber = Convert.ToDouble(Console.ReadLine());
-
-        if (userOption == 1)
-        {
-            double result = firstNumber + secondNumber;
-            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
-        }
-        else if (userOption == 2)
-        {
-            double result = firstNumber - secondNumber;
-            Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
-        }
-        else if (userOption == 3)
-        {
-            double result = firstNumber * secondNumber;
-            Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
-        }
-    }
-    else if (userOption == 7)
-    {
-        Console.WriteLine("Enter the number:");
-        firstNumber = Convert.ToDouble(Console.ReadLine());
-
-        double result = System.Math.Sqrt(firstNumber);
-        Console.WriteLine($"√{firstNumber} = {result}");
-    }
-
+    Console.WriteLine("Bye!");
+    // break;
 }
+else if (userOption == 7)
+{
+    Console.WriteLine("Enter the number:");
+    double number = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"√{number} = {Math.Sqrt(number)}");
+}
+else if (userOption >= 1 && userOption <= 6)
+{
+    Console.WriteLine("Enter the first number:");
+    double firstNumber = Convert.ToDouble(Console.ReadLine());
 
-// else {break;}
+    Console.WriteLine("Enter the second number:");
+    double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+    switch (userOption)
+    {
+        case 1:
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            break;
+
+        case 2:
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            break;
+
+        case 3:
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+            break;
+
+        case 4:
+            if (secondNumber == 0)
+                Console.WriteLine("Cannot divide by zero");
+            else
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            break;
+
+        case 5:
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+            break;
+
+        case 6:
+            Console.WriteLine($"{firstNumber} ^ {secondNumber} = {Math.Pow(firstNumber, secondNumber)}");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid option");
+}
--- starter/obj/Debug/net10.0/starter.AssemblyInfo.cs
+++ starter/obj/Debug/net10.0/starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c189c8e516bfadf0df57c79cf826a52c7577d7d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
